4f00f0265bc53557725ab529e6625a33823e0898
Richard Moser <rmoser@maets.net>
2020-09-16 08:48:39 -0400

    DB Starter - Rev 2

--- db/init_db.js
+++ db/init_db.js
@@ -23,9 +23,11 @@ async function buildTables() {
 
     // build tables in correct order
     await client.query(`
-      CREATE TABLE cheeses (
+      CREATE TABLE products (
         id SERIAL PRIMARY KEY,
         name VARCHAR (255) UNIQUE NOT NULL,
+        type VARCHAR (255),
+        ADD CONSTRAINT type CHECK (type = "Cheese" or type = "Meat" or type = "Fruit" or type = "Pre-Made Board"),
         origin VARCHAR (255),
         description TEXT,
         price NUMERIC (6, 2),
@@ -36,49 +38,31 @@ async function buildTables() {
         id SERIAL PRIMARY KEY,
         "firstName" VARCHAR (255),
         "lastName" VARCHAR (255),
-      );
-      CREATE TABLE orders (
-        id SERIAL PRIMARY KEY,
-        quantity INTEGER,
-        urgency VARCHAR (255),
-      );
-      CREATE TABLE addresses (
-        id SERIAL KEY,
+        email VARCHAR (255) UNIQUE NOT NULL,
+        password VARCHAR (255),
         "houseNumber" INTEGER,
         "streetName" VARCHAR (255),
         city VARCHAR (255),
         state VARCHAR (255),
-        zipcode INTEGER
+        zipcode INTEGER,
+        admin BOOLEAN,
       );
-      CREATE TABLE payments (
-        id SERIAL KEY,
-        "paymentType" VARCHAR (255),
-        ADD CONSTRAINT "paymentType" CHECK ("paymentType" = "VISA" or "paymentType" = "MasterCard" or "paymentType" = "American Express"),
-        "cardNumber" INTEGER,
-        "cardExpiration" DATE,
-        cvv INTEGER
+      CREATE TABLE orders (
+        id SERIAL PRIMARY KEY,
+        user VARCHAR (255) REFERENCES users (email),
+        item VARCHAR (255) REFERENCES products (name),
+        quantity INTEGER,
+        subtotal NUMERIC (6, 2),
+        modifier1 NUMERIC (3, 2),
+        modifier2 NUMERIC (3, 2),
+        modifier3 NUMERIC (3, 2),
+        modifier4 NUMERIC (3, 2),
+        shipping NUMERIC (5, 2),
+        handling NUMERIC (5, 2),
+        total Numeric (6, 2),
+        urgency VARCHAR (255)
       );
-      CREATE TABLE users_addresses (
-        id SERIAL PRIMARY KEY,
-        "usersId" INTEGER REFERENCES users(id),
-        "addressesId" INTEGER REFERENCES addresses(id)
-      );   
-      CREATE TABLE users_orders (
-        id SERIAL PRIMARY KEY,
-        "usersId" INTEGER REFERENCES users(id),
-        "ordersId" INTEGER REFERENCES orders(id)
-      );  
-      CREATE TABLE users_payments (
-        id SERIAL PRIMARY KEY,
-        "usersId" INTEGER REFERENCES users(id),
-        "paymentsId" INTEGER REFERENCES payments(id)
-      );  
-      CREATE TABLE orders_cheeses (
-        id SERIAL PRIMARY KEY,
-        "ordersId" INTEGER REFERENCES orders(id),
-        "cheesesId" INTEGER REFERENCES cheeses(id)
-      );  
-    `)
+     `)
 
   } catch (error) {
     throw error;
