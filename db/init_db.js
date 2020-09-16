f2b93bac6981792e955ada155ba270f74a37a03e
Sebastian <SebastianGillespie99@gmail.com>
2020-09-16 10:44:04 -0400

    added createUserx2

--- db/init_db.js
+++ db/init_db.js
@@ -18,7 +18,17 @@ async function buildTables() {
 
     // build tables in correct order
     await client.query(`
-      
+      CREATE TABLE products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR (255) UNIQUE NOT NULL,
+        type VARCHAR (255),
+        ADD CONSTRAINT type CHECK (type = "Cheese" or type = "Meat" or type = "Fruit" or type = "Pre-Made Board"),
+        origin VARCHAR (255),
+        description TEXT,
+        price NUMERIC (6, 2),
+        hardness VARCHAR (255),
+        odor VARCHAR (255)
+      );
       CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         "firstName" VARCHAR (255),
@@ -30,10 +40,26 @@ async function buildTables() {
         city VARCHAR (255),
         state VARCHAR (255),
         zipcode INTEGER,
-        admin BOOLEAN
+        admin BOOLEAN,
       );
-    
-     `);
+      CREATE TABLE orders (
+        id SERIAL PRIMARY KEY,
+        user VARCHAR (255) REFERENCES users (email),
+        item VARCHAR (255) REFERENCES products (name),
+        quantity INTEGER,
+        status VARCHAR (255),
+        ADD CONSTRAINT status CHECK (status = "Empty Cart" or status = "Incompleted/Abandoned Cart" or status = "Completed Order"),
+        subtotal NUMERIC (6, 2),
+        modifier1 NUMERIC (3, 2),
+        modifier2 NUMERIC (3, 2),
+        modifier3 NUMERIC (3, 2),
+        modifier4 NUMERIC (3, 2),
+        shipping NUMERIC (5, 2),
+        handling NUMERIC (5, 2),
+        total Numeric (6, 2),
+        urgency VARCHAR (255)
+      );
+     `)
 
   } catch (error) {
     throw error;
