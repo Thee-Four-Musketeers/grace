89b995e16e43ef759948c3ca42652173591ac680
coyotewind <coyoteswind@gmail.com>
2020-09-16 11:42:42 -0400

    commits db table clean & create product

--- db/index.js
+++ db/index.js
@@ -7,24 +7,36 @@ const client = new Client(DB_URL);
 // database methods
 
 async function createUser({username, password}){
-  try {
-    const {
-      rows: [ user ]  
-    } = await client.query(`
-    INSERT INTO users (username, password)
-    VALUES ($1, $2)
-    RETURNING *;`,[username, password])
-
-    return user;
-  } catch (error) {
-    throw error
-  }
+    try {
+        const { rows: [user]  } = await client.query(`
+            INSERT INTO users (username, password)
+            VALUES ($1, $2)
+            ON CONFLICT (username) DO NOTHING 
+            RETURNING *;
+        `,[username, password]);
+        return user;
+    } catch (error) {
+        throw error
+    }
 }
 
+async function createProduct({ name, description, price, type }){
+    try {
+        const { rows: [product]  } = await client.query(`
+            INSERT INTO products (name, description, price, type)
+            VALUES ($1, $2, $3, $4)
+            ON CONFLICT (name) DO NOTHING 
+            RETURNING *;
+        `,[name, description, price, type]);
+        return product;
+    } catch (error) {
+        throw error
+    }
+}
 
 // export
 module.exports = {
   client,
-  createUser
-  // db methods
+  createUser,
+  createProduct
 }
