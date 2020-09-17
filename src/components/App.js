9c2d7e56367d3a24ebeaf12317d95306e189a9ff
aeon077 <carolyn.schiltz@gmail.com>
2020-09-16 20:01:27 -0400

    Added back App.js

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+import { Header, Navigation } from './index'
+
+const App = () => {
+
+
+    return (
+        <>
+            <Header />
+            <Navigation />
+        </>
+    )
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {
-  Header, Navigation
-} from './components';
-
-const App = () => {
-
-
-  return (
-    <>
-      <Header />
-      <Navigation />
-    </>
-  )
-}
+  App
+} from './components/index';
 
 ReactDOM.render(
   <App />,
