c5a8a02cacedfd56230209f37f316d362df2c5e4
Sebastian <SebastianGillespie99@gmail.com>
2020-09-17 18:17:21 -0400

    added axios for users

--- src/api/index.js
+++ src/api/index.js
@@ -8,3 +8,42 @@ export async function getSomething() {
     throw error;
   }
 }
+
+
+// trying without local storage:
+
+export async function register({ username, password }) {
+  try {
+      const { data: { user: newUser } } = await axios.post("/api/users/register", {
+          username: username,
+          password: password,
+      });
+      let user = newUser;
+      // if (newUser) {
+      //     localStorage.setItem("user", JSON.stringify(newUser));
+      //     return newUser;
+      // } else {
+      //     alert("Please login to access these features.");
+      // }
+
+      return newUser
+  } catch (error) {
+      throw error;
+  }
+}
+export async function login({ username, password }) {
+  try {
+      const { data: { user } } = await axios.post('api/users/login', {
+          username,
+          password
+      });
+      // if (user) {
+      //     localStorage.setItem('user', JSON.stringify(user));
+      //     return user;
+      // }
+
+      return user 
+  } catch (error) {
+      throw error;
+  }
+}
