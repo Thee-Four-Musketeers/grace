487950ff28c50228744cda8621317c00e9e87ddb
coyotewind <coyoteswind@gmail.com>
2020-09-18 18:49:56 -0400

    footer stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav'
+
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <Navbar>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contactg">Contact</Nav.Link>
-        </Navbar>
+        <Container id="footer" className="px-0" fluid={true}>
+            <Row className="m-auto">
+                <Col>
+                    <Navbar>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#contactg">Contact</Nav.Link>
+                    </Navbar>
+                </Col>
+            </Row>
+        </Container>
+        
     )
 }
 
