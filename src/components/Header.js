3f3f04d2dfdf77ba43918d4ea2b716f11b91de9b
coyotewind <coyoteswind@gmail.com>
2020-09-18 10:35:36 -0400

    updates to nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,44 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+
 // import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import './Header.css'
 
 const Header = () => {
     return (
+        <Container id="header" fluid={true}>
+            <Row>
+                <Col>
+                    <Navbar className="header-absolute bg-dark-overlay px-4" collapseOnSelect expand="lg">
+                        <Navbar.Brand className="mr-4 pr-2" href="#home">
+                            <img className="nav-logo" src="images/CheezyLogo_white.png" alt="Cheezy Logo" />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link href="#cheese">Our Cheeses</Nav.Link>
+                                <Nav.Link href="">Premade Boards</Nav.Link>
+                                <Nav.Link href="">Accompaniments</Nav.Link>
+                            </Nav>
+                            <Nav>
+                                <Nav.Link href="#login">Login</Nav.Link>
+                                <span className="separator px-4 py-2"> | </span>
+                                <Nav.Link eventKey={2} href="#">
+                                    <i className="fas fa-shopping-cart cart-icon"></i>
+                                    <span className="cart-count">33</span>
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Col>
+            </Row>
+        </Container>
 
-        <Navbar className="header-absolute bg-dark-overlay" collapseOnSelect expand="lg" variant="dark">
-            <Navbar.Brand href="#home"> <img className="navLogo" src="images/CheezyLogo_white.png" alt="cheezylogo" /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="">Premade Boards</Nav.Link>
-                    <Nav.Link href="">View our Cheeses</Nav.Link>
-                    <Nav.Link href="">Accompaniments</Nav.Link>
-
-                </Nav>
-                <Nav>
-                    <Nav.Link href="#login">Login</Nav.Link>
-                    <Nav.Link eventKey={2} href="">
-                        <i className="fas fa-shopping-cart"></i>
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
         // <Navbar className="header-absolute bg-dark-overlay" collapseOnSelect expand="xl" sticky="top">
         //     <img className="navLogo" src="images/CheezyLogo_white.png" alt="cheezylogo" />
         //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
