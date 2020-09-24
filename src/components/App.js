fcca4d19e671caf064450b3c69e67fafb1534a51
coyotewind <coyoteswind@gmail.com>
2020-09-24 17:33:09 -0400

    structuring row, cols, cards

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from "../components/Header";
+import Title from "../components/Title";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+
 import Home from "../pages/Home";
 import Cheeses from "../pages/Cheeses";
 import Boards from "../pages/Boards";
 import Sides from "../pages/Sides";
-import Footer from "../components/Footer";
+
+import { Container, Row, Col } from 'react-bootstrap';  
 
 import './App.css'
 
@@ -33,8 +38,6 @@ const App = () => {
         setUser(localStorageUser());
     }, []);
 
-    // do we pass a type or not?
-
     useEffect(() => {
         fetchProductsByType(productType)
             .then((response) => {
@@ -46,9 +49,6 @@ const App = () => {
             });
     }, []);
 
-
-    // test with user and without
-
     return (
         <>
             <Router>
@@ -56,18 +56,43 @@ const App = () => {
                 <div id="all">
                     <main>
                         <Switch>
+
                             <Route path="/cheeses">
-                                <Cheeses products={products} setProductType={setProductType} />
+                                <Title title={'Our Cheeses'} />
+                                <Container id="wrapper" fluid>
+                                    <Row>
+                                        <Sidebar />
+                                        <Cheeses products={products} setProductType={setProductType} />
+                                    </Row>
+                                </Container>
                             </Route>
+
                             <Route path="/boards">
-                                <Boards products={products} setProductType={setProductType} />
+                                <Title title={'Pre-Made Boards'} />
+                                <Container id="wrapper" fluid>
+                                    <Row>
+                                        <Sidebar />
+                                        <Boards products={products} setProductType={setProductType} />
+                                    </Row>
+                                </Container>
+                                
                             </Route>
+
                             <Route path="/sides">
-                                <Sides products={products} setProductType={setProductType} />
+                                <Title title={'Accompaniments'} />
+                                <Container id="wrapper" fluid>
+                                    <Row>
+                                        <Sidebar />
+                                        <Sides products={products} setProductType={setProductType} />
+                                    </Row>
+                                </Container>
+                                
                             </Route>
+
                             <Route path="/">
                                 <Home />
                             </Route>
+
                         </Switch>
                     </main>
                     <Footer />
@@ -75,6 +100,7 @@ const App = () => {
             </Router>
         </>
     )
+    
 };
 
 export default App;
