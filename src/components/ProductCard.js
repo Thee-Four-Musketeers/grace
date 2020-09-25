55ca4cf3ea7e9dd097c50240863b024fdcc21db5
coyotewind <coyoteswind@gmail.com>
2020-09-25 00:38:12 -0400

    comitting many changes to header and footer

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -7,17 +7,24 @@ import './ProductCard.css'
 
 const ProductCard = ({ id, name, type, description }) => {
 
+    function shorten(str,n) {
+        return (str.match(RegExp(".{"+n+"}\\S*"))||[str])[0];
+    }
+
     return (
         <Card className="text-center" key={id}>
             <Link className="" to="/{type}/{id}">
-                <Card.Img variant="top" src="images/Cheeseplate2.jpg" />
+                <Card.Img variant="top" src="images/Hero_CheesePlate_1.jpg" />
             </Link>
             <Card.Body>
                 <Link className="" to="/{type}/{id}">
-                    <Card.Title className="pb-3">{name}</Card.Title>
+                    <Card.Title className="pb-1">{name}</Card.Title>
                 </Link>
-                <Button variant="primary">Add To Cart</Button>
+                <Card.Text className="pb-1">{ shorten(description, 60) + '...'}</Card.Text>
             </Card.Body>
+            <Card.Footer className="pb-4 pt-0">
+                <Button variant="primary" className="btn-card">Add To Cart</Button>
+            </Card.Footer>
         </Card>
     );
     
