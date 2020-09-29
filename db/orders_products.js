1cbbd81c537a54e43d95016bdd92855cb2b54f56
coyotewind <coyoteswind@gmail.com>
2020-09-29 17:56:31 -0400

    meaningless changes

---
+++ db/orders_products.js
@@ -0,0 +1,53 @@
+const client = require('./client');
+// const util = require('./util');
+
+
+
+async function createCartItem({ productId, orderId, productIdQuantity }) {
+    try {
+        const { rows: [cart] } = await client.query(`
+        INSERT INTO orders_products ("productId", "orderId", "productIdQuantity")
+            VALUES ($1, $2, $3)
+            ON CONFLICT ("productId") DO NOTHING 
+            RETURNING *;
+        `, [productId, orderId, productIdQuantity]);
+        return cart;
+    } catch (error) {
+        throw error
+    }
+}
+
+// async function createOrderProduct(orderId, productId) {
+//     try {
+//         const {
+//             rows: [joint],
+//         } = await client.query(`
+//             INSERT INTO orders_products ("orderId", "productId")
+//             VALUES ($1, $2)
+//             ON CONFLICT ("orderId", "productId") DO NOTHING
+//             RETURNING *;
+//         `, [orderId, productId]
+//         );
+//         return joint;
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+
+// async function addProductsToOrder(orderId, productList) {
+//     try {
+//         const createProductList = productList.map(
+//             product => createOrderProduct(orderId, product.id)
+//         );
+//         await Promise.all(createProductList);
+//         return await getOrderById(orderId);
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+
+module.exports = {
+    createCartItem
+    // addProductsToOrder,
+    // createOrderProduct
+};
