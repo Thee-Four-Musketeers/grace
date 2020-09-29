1af009ef9510db7cc1991e4404ca4d185d4675c6
Richard Moser <rmoser@maets.net>
2020-09-29 11:54:08 -0400

    moved things for consistency - deleted /api/utils.js

--- routes/utils.js
+++ routes/utils.js
@@ -1,6 +1,8 @@
 // make a require admin
 // check the boolean user table isAdmin
 
+const user = require('../db')
+
 function requireUser(req, res, next) {
     if (!req.user) {
         next({
@@ -10,6 +12,18 @@ function requireUser(req, res, next) {
     }
     next();
 }
+
+function requireAdmin(req, res, next) {
+    if (user.Admin !== true) {
+      next({
+        name: "User is not Admin",
+        message: "You must be an Admin to perform this action"
+      });
+    }
+  
+    next();
+  }
+
 module.exports = {
-    requireUser
+    requireUser, requireAdmin
 }
